cf68ff4e3d43acdfcdbaa3221e4cdbbe77fc256d
Alessiavals <alessiavalentini96@gmail.com>
2025-01-23 14:37:20 +0100

    first commit

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+    }
+}
