2bcfa4edf630c306f0833142e2687f56497c2495
Alessiavals <alessiavalentini96@gmail.com>
2025-01-23 15:06:06 +0100

    first commit

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class PasswordGenerator {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci il nome");
+        String nome = scan.nextLine();
+
+        System.out.println("Inserisci il cognome");
+        String cognome = scan.nextLine();
+
+        System.out.println("Inserisci colore preferito");
+        String colore = scan.nextLine();
+
+        System.out.println("Inserisci data di nascita");
+        int dataNascita = scan.nextInt();
+
+        System.out.println("La password generata è " + nome + "-" + cognome + "-" + colore + "-" + dataNascita);
     }
 }
